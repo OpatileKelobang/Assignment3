b65771f98b2d433539a17e6c27c171eeba7e8969
OpatileKelobang <49315650+OpatileKelobang@users.noreply.github.com>
2020-03-04 08:36:58 +0200

    initial commit

---
+++ src/main/java/com/example/Employee.java
@@ -0,0 +1,44 @@
+package com.example;
+
+public class Employee extends Person {
+
+    private String jobTitle;
+    private double salary;
+
+    public Employee()
+    {
+
+    }
+
+    public Employee(String jobTitle, double salary) {
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    public Employee(String name, String age, String jobTitle, double salary) {
+        super(name, age);
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    private double calculateSalary(double tax)
+    {
+        return getSalary() / tax;
+    }
+}
